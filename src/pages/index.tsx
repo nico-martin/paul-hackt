7be17e49860eeeffb7c34620efd0760245bf0f26
rotwurstesser <raphael.antonitti@gmail.com>
2023-05-12 17:57:03 +0200

    First inputs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,62 @@
 import { useForm } from 'react-hook-form';
-import { Form, FormElement, InputCheckbox } from "@theme";
+import { Form, FormElement, InputCheckbox, InputText } from "@theme";
+import { useState } from 'react';
 
-const Home: NextPage = () => {
-  const { handleSubmit, control } = useForm();
+const Home = () => {
+  const form = useForm();
+  const [isGrownUp, setIsGrownUp] = useState(false);
 
-  const handleFormSubmit = (data: any) => {
+  const handleFormSubmit = (data) => {
     console.log(data);
     // Handle form submission
   };
 
-  const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const value = event.target.checked;
-    console.log(value);
-    // Handle checkbox value change
+  const handleCheckboxChange = () => {
+    setIsGrownUp((prevValue) => !prevValue);
   };
+
+
 
   return (
     <div className="flex items-center justify-center h-screen">
-      <Form onSubmit={handleSubmit(handleFormSubmit)} className="p-4 rounded-lg shadow-md">
+      <Form onSubmit={form.handleSubmit(handleFormSubmit)} className="p-4 rounded-lg shadow-md">
+        <FormElement
+          label="What is your name?"
+          Input={InputText}
+          name="name"
+          form={form}
+        />
+
         <FormElement
           label="Are you a grown up?"
           Input={InputCheckbox}
           name="isGrownUp"
-          form={{ control }} // Pass the control object as a prop
+          form={form}
+          onChange={handleCheckboxChange}
         >
           <InputCheckbox
             name="isGrownUp"
-            onChange={handleCheckboxChange} // Update the onChange prop
+            checked={isGrownUp}
+            onChange={handleCheckboxChange}
           />
         </FormElement>
-        {/* Other form elements */}
+
+        {isGrownUp ? (
+          <FormElement
+            label="What do you work or study as?"
+            Input={InputText}
+            name="occupation"
+            form={form}
+          />
+        ) : (
+          <FormElement
+            label="What do you want to be when you grow up?"
+            Input={InputText}
+            name="dreamJob"
+            form={form}
+          />
+        )}
+
         <button type="submit" className="px-4 py-2 mt-4 text-white bg-blue-500 rounded-lg hover:bg-blue-600">
           Submit
         </button>
