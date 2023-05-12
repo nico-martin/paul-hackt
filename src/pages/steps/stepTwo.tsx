5a0bc6396a327b12e57a773d18bc52500a3b0bc0
Jonas Niestroj <j.niestroj@protonmail.ch>
2023-05-12 20:09:09 +0200

    feat: add greetings text

--- src/pages/steps/stepTwo.tsx
+++ src/pages/steps/stepTwo.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { usePerson } from '@/store/PersonContext';
-
 
 const StepTwo = () => {
   const [person, setPerson] = usePerson();
+  const [greetingsText, setGreetingsText] = useState();
 
-  const handleCheckboxChange = (event:any) => {
+  const handleCheckboxChange = (event: any) => {
     const { checked } = event.target;
     setPerson({
       ...person,
@@ -13,17 +13,28 @@ const StepTwo = () => {
     });
   };
 
+  useEffect(() => {
+    fetch(`/api/greetings?name=${person.name}`).then(async (response) => {
+      const json = await response.json();
+      setGreetingsText(json.message);
+    });
+  }, []);
+
   return (
     <div>
-      <p>Are you a grown-up?</p>
-      <label className="switch">
-        <input
-          type="checkbox"
-          checked={person.isGrownUp || false}
-          onChange={handleCheckboxChange}
-        />
-        <span className="slider round"></span>
-      </label>
+      <div>{greetingsText}</div>
+      <br />
+      <div>
+        <p>Are you a grown-up?</p>
+        <label className="switch">
+          <input
+            type="checkbox"
+            checked={person.isGrownUp || false}
+            onChange={handleCheckboxChange}
+          />
+          <span className="slider round"></span>
+        </label>
+      </div>
     </div>
   );
 };
