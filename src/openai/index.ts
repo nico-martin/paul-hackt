450f7badf019dd51ccc5f4d7a9aaa2753fb86d52
Jonas Niestroj <j.niestroj@protonmail.ch>
2023-05-12 20:25:50 +0200

    feat: add openai system messages

--- src/openai/index.ts
+++ src/openai/index.ts
@@ -1,20 +1,37 @@
-import { Configuration, OpenAIApi } from 'openai';
+import { ChatCompletionRequestMessage, Configuration, OpenAIApi } from 'openai';
 
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
+export default async (
+  prompt: string,
+  sendSystemPrompt = true,
+  systemPromptAdd = ''
+) => {
+  try {
+    const messages: ChatCompletionRequestMessage[] = [];
+    if (sendSystemPrompt) {
+      const basePrompt =
+        'Du bist «Lily» ein freuntlicher Audioguide der hilfreich durch eine Ausstellung im Zentrum Paul Klee bei Bern (Schweiz) führt. Das Zentrum Paul Klee ist ein Museum. Du sprichst immer als Lily. Die Leute benutzen den Audioguide Lily im Museum und stehen vor den Werken.';
 
-export default async (prompt: string) => {
-    try {
-        const response = await openai.createChatCompletion({
-            model: 'gpt-3.5-turbo',
-            messages: [{ role: 'user', content: prompt }]
-        });
-        return response.data.choices?.[0].message?.content
-    } catch (e: any) {
-        console.log(e.response.data);
+      messages.push({
+        role: 'system',
+        content: basePrompt + systemPromptAdd,
+      });
     }
-    return undefined
-}
+    messages.push({ role: 'user', content: prompt });
+
+    const response = await openai.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      messages: messages,
+      temperature: 0.4,
+    });
+
+    return response.data.choices?.[0].message?.content;
+  } catch (e: any) {
+    console.log(e.response.data);
+  }
+  return undefined;
+};
